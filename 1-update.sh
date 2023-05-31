564cd8b8b6291e4cadeceda3a6516b904bf24c15
NaitLee <naitli@foxmail.com>
2023-05-31 20:47:36 +0800

    fix trailing quote in po files

--- 1-update.sh
+++ 1-update.sh
@@ -2,6 +2,7 @@
 xonotic_data="/mnt/data/@/git-repo/xonotic/data/xonotic-data.pk3dir/"
 cp common.zh_Hant.po common.zh_Hant.po.old
 cat header.po >new.po
+#FIXME: THIS IS WORKING SO BAD. maybe rewrite with python or else
 msgcat common.zh_Hant.po "$xonotic_data/common.pot" | awk -f afterprocess.awk | tail -n +2 >>new.po
 echo "\"" >>new.po
 mv new.po common.zh_Hant.po
