334926e355be7509a4b5b470439c6a1487e8b075
NaitLee <naitli@foxmail.com>
2023-06-09 09:32:03 +0800

    modify translation of “hunt” (survival)

--- 0-convert.sh
+++ 0-convert.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 for lang in zh_CN zh_HK zh_TW; do {
+    echo $lang
     opencc -c $lang.json -i common.zh_Hant.po >common.$lang.po
     opencc -c $lang.json -i README.zh_Hant.md >README.$lang.md
 }; done
